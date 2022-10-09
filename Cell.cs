06c922055af90a0dd099ed2fddc7650456ec2009
Nazar <nazariykharchuk@gmail.com>
2022-10-09 11:32:58 +0300

    Added Cell class

---
+++ Cell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIIS_labs
+{
+    class Cell : IEquatable<Cell>
+    {
+        public int col { get; set; }
+        public int row { get; set; }
+
+        public Cell()
+        {
+            col = 0;
+            row = 0;
+        }
+
+        public Cell(int _col, int _row)
+        {
+            col = _col;
+            row = _row;
+        }
+
+        public bool Equals(Cell other)
+        {
+            return this.col == other.col && this.row == other.row;
+        }
+    }
+}
