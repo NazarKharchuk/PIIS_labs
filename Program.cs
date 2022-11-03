58f092fde7c4966197cebf1e00364790b634524a
Nazar <nazariykharchuk@gmail.com>
2022-11-03 18:35:32 +0200

    Added Karp-Rabin algorithm

--- Program.cs
+++ Program.cs
@@ -10,6 +10,37 @@ namespace PIIS_labs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\t\tLab 4\n" +
+                "Please select an algorithm:\n" +
+                "1 - Rabin - Karpa algorithm\n" +
+                "2 - Dijkstra's algorithm\n" +
+                "3 - Prim's algorithm");
+
+            int algo = Int32.Parse(Console.ReadLine());
+
+            switch (algo)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("\tRabin - Karpa algorithm:");
+                    Console.WriteLine("Please enter text to search:");
+                    string text = Console.ReadLine();
+                    Console.WriteLine("Please enter the template you are looking for:");
+                    string template = Console.ReadLine();
+                    RabinKarpAlgorithm rabin_karp = new RabinKarpAlgorithm(text, template);
+                    Console.WriteLine("Count - " + rabin_karp.search());
+                    break;
+                case 2:
+                    Console.WriteLine("\tDijkstra's algorithm:");
+                    break;
+                case 3:
+                    Console.WriteLine("\tPrim's algorithm:");
+                    break;
+                default:
+                    Console.WriteLine("Invalid algorithm(");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ RabinKarpAlgorithm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIIS_labs
+{
+    class RabinKarpAlgorithm
+    {
+        private string text;
+        private string template;
+
+        public RabinKarpAlgorithm(string _text, string _template)
+        {
+            text = _text;
+            template = _template;
+        }
+
+        public int search()
+        {
+            int count = 0;
+            int template_heuristic = calculate_hash(template);
+            int text_heuristic;
+            string sub_str;
+
+            for (int i = 0; i < (text.Length - template.Length + 1); i++)
+            {
+                sub_str = text.Substring(i, template.Length);
+                text_heuristic = calculate_hash(sub_str);
+                if(template_heuristic == text_heuristic)
+                {
+                    if(sub_str == template)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public int calculate_hash(string str)
+        {
+            int digit = 0;
+            int num;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                num = (int)str[i];
+                /*if (num < 48 || num > 57)
+                {
+                    for(int j = 0; j < num.ToString().Length; j++)
+                    {
+                        digit *= 10;
+                    }
+                    digit += num;
+                }
+                else
+                {
+                    digit = digit * 10 + num - 48;
+                }*/
+                digit += num;
+            }
+            //Console.WriteLine(str + " - " + digit);
+            return (digit % 25);
+        }
+    }
+}
